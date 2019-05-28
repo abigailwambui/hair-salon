15b9ce4ed83693d31c23851b9b1eaad9a7a41e88
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-28 12:15:33 +0300

    add a route that leads directly to clients

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -102,6 +102,15 @@ public class App{
           return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        get("/clients/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Client client = Client.find(Integer.parseInt(request.params(":id")));
+            model.put("client", client);
+            model.put("template", "templates/client.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
         get("/stylists/:stylist_id/clients/:id", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
