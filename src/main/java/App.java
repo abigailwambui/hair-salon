2451682937e49880e48ab087918da4628ac2d0c5
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-29 16:39:46 +0300

    change database url to fit heroku url provided

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,21 +4,22 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
+
 public class App{
-     public static void main(String[] args) {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    public static void main(String[] args){
+
+        port(getHerokuAssignedPort());
+
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
-
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-     if (process.environment().get("PORT") != null) {
-         port = Integer.parseInt(process.environment().get("PORT"));
-     } else {
-         port = 4567;
-     }
-
-    port(port);
-
 
         get("/", (request,response) -> {
             Map<String, Object> model = new HashMap<String,Object>();
