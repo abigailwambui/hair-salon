722fc4e17f9d2657b4f17bf1c5dab4787e467808
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-29 18:33:24 +0300

    include changes to enable a push to heroku

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,7 +3,11 @@ import org.sql2o.*;
 public class DB {
   //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "abigail", "123");
 
-    public static Sql2o sql2o = new Sql20("jdbc:postgresql://ec2-107-20-155-148.compute-1.amazonaws.com:5432/d8pestug7sj6vj", "ianllhticdknqi", "7c82d0ddb4076d9cb2856cc784e7e3907be60b14e0abb907360254f503704cb1");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-107-20-155-148.compute-1.amazonaws.com:5432/d8pestug7sj6vj", "ianllhticdknqi", "7c82d0ddb4076d9cb2856cc784e7e3907be60b14e0abb907360254f503704cb1");
     
+
+
+
+
 
 }
